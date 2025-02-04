f8181f1b730306f9521bc91c253df75e68f6dfde
Shubham Yadav <159351522+Bunny2208@users.noreply.github.com>
2025-02-04 17:18:40 +0000

    commit

---
+++ 01/variables.js
@@ -0,0 +1,11 @@
+const accountId=1111
+let accountName = "HDFC"
+var accountPassword= "12345"
+accountCity= "jaipur"
+let accountState ;
+
+
+
+
+console.log(accountId);
+console.table([accountId,accountName,accountPassword,accountCity,accountState]);
