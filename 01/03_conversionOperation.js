fea5d63ecdca0ec73d236f2b20cd36ed11de3cd6
Shubham Yadav <159351522+Bunny2208@users.noreply.github.com>
2025-02-04 19:10:27 +0000

    Conversion and operation

--- 01/03_conversionOperation.js
+++ 01/03_conversionOperation.js
@@ -24,5 +24,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
- console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//-------------------- operations------------------------------
+
+
+// console.log(2+3);
+// console.log(4-3);
+// console.log(2*3);
+// console.log(6/3);
+// console.log(21%3);
+
+let str1 = "shub"
+let str2 = "ham"
+
+let str3 = str1+str2;
+//console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
