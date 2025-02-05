d041cde3f454ab2a8a3e7dd01d05864becf59aab
Shubham Yadav <159351522+Bunny2208@users.noreply.github.com>
2025-02-05 20:39:34 +0000

    first compare

---
+++ 01/04_comparisionop.js
@@ -0,0 +1,14 @@
+//operators 
+// < : less then 
+// > : greater then 
+// <= : less then equal to 
+// >= : greater then equal to
+// == equal too 
+// === strickly equal too 
+
+console.log(null < 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null == 0);
+
