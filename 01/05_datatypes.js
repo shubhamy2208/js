86b0f803ee54052e020b00a67d7d8b79c30288dd
Shubham Yadav <159351522+Bunny2208@users.noreply.github.com>
2025-02-05 21:08:34 +0000

    DataTypes summary

---
+++ 01/05_datatypes.js
@@ -0,0 +1,25 @@
+// primative data types 
+// 7 types : string,boolean,number ,null ,undefined,symbol,BigInt
+
+const id = 1 ;
+let name = "shubham ";
+let score = 33.3;
+let temp = null;
+let partner; 
+
+// Reference (Non primative datatypes)
+// Array ,objects ,Functions
+
+//Array 
+const heros =['samosa','raga','saktiman'];
+console.log(heros)
+
+//objects 
+
+let myobj = {
+    name : "shivam"
+}
+//functions 
+const myfunction = function(){
+    console.log('haoo');
+}
