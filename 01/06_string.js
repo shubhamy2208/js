6a4d254e4c9879d38e63b0eb181f2a691ec6930c
Shubham Yadav <159351522+Bunny2208@users.noreply.github.com>
2025-02-12 17:46:45 +0000

    commit2

--- 01/06_string.js
+++ 01/06_string.js
@@ -9,7 +9,6 @@ const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
-
  
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
